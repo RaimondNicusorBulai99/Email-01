c25d26b2fa8e518628da99dcf5ad9bec37f41f4d
RaimondnicusorBulai <Condivisionedatiraimond@gmail.com>
2022-11-10 12:32:03 +0100

    finish

---
+++ src/main/java/Servizi/Email1/services/EmailService.java
@@ -0,0 +1,21 @@
+package Servizi.Email1.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendTo(String email, String title, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(title);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
